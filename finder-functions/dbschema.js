2390562f85951a88965fd622c61341b3d650d910
Bob Weichler <bobmasaki@hotmail.com>
2020-02-10 06:41:12 +0100

    Adding Express + reformat get/post scream request

---
+++ finder-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'This is a scream body',
+      createdAt: '2020-02-10T05:36:00.070Z',
+      likeCount: 5,
+      commentCount: 2,
+    }
+  ],
+};
--- finder-functions/functions/index.js
+++ finder-functions/functions/index.js
@@ -3,19 +3,22 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world!");
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          ...doc.data()
+        });
       });
       return res.json(screams);
     })
@@ -24,14 +27,11 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if(req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed' });
-  }
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin.firestore().collection('screams').add(newScream)
@@ -43,3 +43,6 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+// ROUTES
+exports.api = functions.region('europe-west1').https.onRequest(app);
