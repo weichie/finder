8332960e4dd37ed17b3e01d485cceceb512c1edf
Bob Weichler <bobmasaki@hotmail.com>
2020-02-10 07:13:21 +0100

    User registration setup

--- finder-functions/functions/index.js
+++ finder-functions/functions/index.js
@@ -1,14 +1,20 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-  admin
-    .firestore()
+  db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -44,5 +50,52 @@ app.post('/scream', (req, res) => {
     });
 });
 
-// ROUTES
+// Signup Routes
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`).get()
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'This handle is already taken' });
+      } else {
+        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+
+      return db.doc(`/user/${newUser.handle}`).set(userCredentials);
+    })
+    .then(data => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ error: 'The email address is already in use by another account.' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
