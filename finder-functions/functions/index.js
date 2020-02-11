403777c3c6bea4483e187373cd1aa53bcf2a4616
Bob Weichler <bobmasaki@hotmail.com>
2020-02-11 06:45:56 +0100

    Login route

--- finder-functions/functions/index.js
+++ finder-functions/functions/index.js
@@ -50,6 +50,17 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
+
 // Signup Routes
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -58,6 +69,19 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+  
+  let errors = {};
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   //TODO: validate data
   let token, userId;
@@ -97,5 +121,36 @@ app.post('/signup', (req, res) => {
     });
 });
 
+// Login
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  }
+
+  let errors = {};
+  if (isEmpty(user.email)) errors.email = 'Must not me empty';
+  if (isEmpty(user.password)) errors.password = 'Must not me empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/user-not-found') {
+        return res.status(403).json({ error: 'This email address does not exist.'});
+      }
+      if (err.code === 'auth/wrong-password') {
+        return res.status(403).json({ error: 'Wrong credentials, please try again.' });
+      }
+      return res.status(500).json({ error: err.code });
+    });
+});
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
